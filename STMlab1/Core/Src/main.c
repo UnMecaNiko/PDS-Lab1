f9e307b36f8e17c2e1e8b67fb8b8c02ab5752583
Nicolas Velasquez <velasquezlopezn@gmail.com>
2022-01-30 00:53:29 -0500

    se configura el adc con timer a 50 ms, se envía un pulso por cada lectura y se envía a terminal, queda por medir qué duración tiene el pulso, esto ayudaría a ver cuanto se demora haciendo la conversión el adc. Se intentó anteriormente dejar el adc en modo continuo con dma pero el timer no funcionaba. El documento se encuentra pendiente a comentarios

--- STMlab1/Core/Src/main.c
+++ STMlab1/Core/Src/main.c
@@ -110,7 +110,7 @@ int main(void)
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
 
-  HAL_TIM_Base_Start_IT(&htim2);
+  HAL_TIM_Base_Start_IT(&htim2); //se debe inicializar timer
   /* USER CODE END 2 */
 
   /* Infinite loop */
